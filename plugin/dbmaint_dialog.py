d14dc4ab7995e0ee0c103c6c7c981007600953ac
solymosin <solymosi.norbert@gmail.com>
2016-06-05 16:14:10 +0200

    0.75

--- plugin/dbmaint_dialog.py
+++ plugin/dbmaint_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dbmaint_dialog_base.ui'
 #
-# Created: Sun Jun  5 07:41:31 2016
+# Created: Sun Jun  5 16:13:28 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
--- plugin/xitem_dialog.py
+++ plugin/xitem_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'xitem_dialog_base.ui'
 #
-# Created: Sun Jun  5 07:41:32 2016
+# Created: Sun Jun  5 16:13:28 2016
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
